eb73170d1dcfb77e7e722c54d05f12f34e946533
Isis <mejaikatwt@gmail.com>
2026-05-23 23:48:06 +0100

    Import Spectre.Console, create new classes: Cells, Controller, PrototypeView and create new interface: IView

---
+++ Blackout/Cells.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blackout
+{
+    public class Cells
+    {
+        private bool isActive;
+        public Cells()
+        {
+            isActive = true;
+        }    
+    }
+}
---
+++ Blackout/PrototypeView.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Spectre.Console;
+
+namespace Blackout
+{
+    public class PrototypeView : IView
+    {
+        public void ShowGrid(int gridSize)
+        {
+            Canvas grid = new Canvas(gridSize,gridSize);
+            for(int y = 0; y < grid.Height; y++)
+            {
+                for(int x = 0; x < grid.Width; x++)
+                {
+                    if((x % 2 == 0 && y % 2 == 0)||(x % 2 != 0 && y % 2 != 0 ))
+                    {
+                        grid.SetPixel(x, y, Color.Aqua);
+                    }
+                    else
+                    {
+                        grid.SetPixel(x, y, Color.DeepPink4_2);
+                    }
+                }
+            }
+
+            grid.MaxWidth = 20;
+
+            AnsiConsole.Write(grid);
+        }
+    }
+}
